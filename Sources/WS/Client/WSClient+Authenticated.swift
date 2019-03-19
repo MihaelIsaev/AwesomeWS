ac4848c7d1c3f72d32a20e29019908dc85c27630
Mihael Isaev <isaev.mihael@gmail.com>
2019-03-19 15:05:38 +0400

    WSClient+Authenticated: name parameters in callback closure

--- Sources/WS/Client/WSClient+Authenticated.swift
+++ Sources/WS/Client/WSClient+Authenticated.swift
@@ -1,7 +1,7 @@
 import Authentication
 
 extension Set where Element == WSClient {
-    public func authenticated<A>(_ type: A.Type = A.self) throws -> [(WSClient, A)] where A: Authenticatable {
+    public func authenticated<A>(_ type: A.Type = A.self) throws -> [(client: WSClient, user: A)] where A: Authenticatable {
         return compactMap {
             if let user = try? $0.req.requireAuthenticated(type) {
                 return ($0, user)
