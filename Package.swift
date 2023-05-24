bc8eda4848dc40b9f471b3026da9b1da33cbb246
Tierra Cero <vcantu01@tierracero.com>
2023-05-24 17:41:25 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0")
+        //.package(url: "https://github.com/vapor/websocket-kit.git", from: "2.0.0")
+        .package(url: "https://github.com/vapor/websocket-kit.git", exact: "2.9.1")
     ],
     targets: [
         .target(name: "WS", dependencies: [
